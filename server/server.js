4fff5a800bbd0096dfabc6831c4259c75373b7be
Stephen M. Wong <stephen.wong@tufts.edu>
2017-10-28 23:00:00 -0400

    get parking data from api

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 var express = require("express");
+var axios = require('axios');
 var app = express();
 var bodyParser = require("body-parser");
+var MapboxClient = require('mapbox');
+var client = new MapboxClient('pk.eyJ1IjoibWVkZm9yZGhpc3RvcmljYWwiLCJhIjoiY2o4ZXNiNHN2MTZycjMzb2ttcWp0dDJ1aiJ9.zt52s3jkwqtDc1I2Fv5cJg');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -12,7 +15,63 @@ app.get('/', function(req, res) {
 });
 
 app.get('/parking', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+    var info= req.query;
+    var lat = info.lat;
+    var long = info.long;
+    var dist = info.dist;
+    var street = info.street;
+    console.log(lat);
+    getParkingData(lat, long, dist, res);
+    // axios.get('https://apis.solarialabs.com/shine/v1/parking-rules/meters',        { 
+    //         params: {
+    //             "lat": lat,
+    //             "long": long,
+    //             "max-distance": dist,
+    //             "max-results": 5,
+    //             "apikey": "xGKeKshQA8IBJsXiOgm0mdGjvWao2VjD"
+    //         }
+    //     })
+    
+    // .then(function(response) {
+    //     //console.log(data);
+    //     //console.log(response.data);
+    //     res.json(response.data);
+    //         //JSON.stringify(data));
+        
+    
+//     })
+// .catch (function(error){
+//         console.log(error);
+//         });
+
 });
 
+function getParkingData(lat, long, dist, res) {
+   axios.get('https://apis.solarialabs.com/shine/v1/parking-rules/meters',        { 
+            params: {
+                "lat": lat,
+                "long": long,
+                "max-distance": dist,
+                "max-results": 5,
+                "apikey": "xGKeKshQA8IBJsXiOgm0mdGjvWao2VjD"
+            }
+        })
+    
+    .then(function(response) {
+        //console.log(data);
+        //console.log(response.data);
+        res.json(response.data);
+            //JSON.stringify(data));
+        
+    
+    })
+.catch (function(error){
+        });
+}
+
+function cmpRankCrime() {
+    
+}    
+
 app.listen(3000);
+
